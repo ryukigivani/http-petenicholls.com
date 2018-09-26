8d79ef740fe6b19220cb6a2e3d0111255b8439e0
ryukigivani <43603066+ryukigivani@users.noreply.github.com>
2018-09-26 09:24:13 +0000

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,48 @@
+<!--Sc By Km1b@k-->
+<html>
+<head>
+<meta charset="UTF-8"
+<meta property="og:image"content="http://thumbs2.imagebam.com/6e/e7/31/f77529965064314.jpg">
+<meta property="og:title" content=" Not|Found">   
+<meta property="og:description" content="Owned By Km1b@k">
+<title>Owned By Km1b@k</title>   
+  <link href="http://fonts.googleapis.com/css?family=Rancho" rel="stylesheet" type="text/css">
+<center>     
+<style>
+body {
+    background-image:url("https://i0.wp.com/blog.smartthings.com/wp-content/uploads/2014/03/tumblr_n1fqbfMefH1smapx8o1_500.gif"); 
+    background-repeat: no-repeat; 
+    background-size: 100% 100%; 
+} 
+</style>
+</head>    
+<body>   
+<script>    alert('Dear Admin!!! :V')   </script>   
+</br>
+</br>
+</br>
+</br>
+<div align="center">
+<p>&nbsp;<img src="https://2.bp.blogspot.com/-Y-foO7FOwqM/WQ2VuUhDfKI/AAAAAAAABfs/rPDJP8SEIsgUiIchfM3TSfTOodS04UOUwCLcB/s1600/gambar-lucu-monster-actionesia-3.png" border="0" width="200" 
+height="200">
+<div>
+<font color="FFFFFF" size="15" face="Rancho">Owned By<font color="#FF0000"> Km1b@k</span><br><br>
+<font face="Rancho" color="aqua" size="4px">Terkadang mereka yang berjuang tidak mendapatkan apa-apa.<br> Sementara yang biasa, mendapatkan banyak<br>Hidup kadang-kadang sangat lucu. ^ _ ^
+</span>
+<center>
+<marquee behavior="alternate" scrollamount="50" width="320"> <font size="4" 
+color="red">________________<font size="4" 
+
+color="yellow">________________<font size="4" 
+
+color="#1CFF00">________________<font size="4" 
+
+color="#00A7FF">________________</font></font></font></font></marquee>
+<center>
+<font face="verdana" size="3" color="white">Greetz :</font><marquee behavior="alternate" scrollamount="13" width="650"><font face="verdana" size="3" color="gold"># Km1b@k -</font>
+<font face="verdana" size="3" color="white"> Kuda.php - 50.fyan - Mr.one - ./13ud1 <font face="verdana" size="3" color="gold">#</font></font></marquee>
+<br>
+<audio autoplay="autoplay" controls="controls" src="https://ryuki.podbean.com/mf/play/fix4uq/deadpool.mp3" type="audio/mpeg"></audio>
+</center>
+</body>
+</html>
